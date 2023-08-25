02fa0749b245f6a2d6023a609eabb05ca4dd2726
Bassant Khaled <bossyomran262@gmail.com>
2023-08-25 16:37:56 -0700

    correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,12 +8,13 @@
 */
 int _strlen(char *s)
 {
-  int i = 0;
-  if (!s)
-    return (0);
-  while (*s++)
-    i++;
-  return (i);
+int i = 0;
+if (!s)
+return (0);
+
+while (*s++)
+i++;
+return (i);
 }
 
 /**
@@ -25,12 +26,12 @@ int _strlen(char *s)
 
 size_t print_list(const list_t *h)
 {
-  size_t i = 0;
-  while (h)
-    {
-      printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "nil");
-      h = h->next;
-      i++;
-    }
-  return (i);
+size_t i = 0;
+while (h)
+{
+printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "nil");
+h = h->next;
+i++;
+}
+return (i);
 }
